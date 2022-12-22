4e1c8ff80282b88b308d20211303099a4b6a7efd
Claudio Jose Bezerra Filho <65622389+claudiojbf@users.noreply.github.com>
2022-12-22 14:22:57 -0300

    Primeiros codigos das aulas

---
+++ Calculator/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("O que deseja fazer?");
+            System.Console.WriteLine("1 - Soma");
+            System.Console.WriteLine("2 - Subtracao");
+            System.Console.WriteLine("3 - Multiplicacao");
+            System.Console.WriteLine("4 - Divisao");
+            System.Console.WriteLine("5 - Sair");
+            System.Console.WriteLine("---------------");
+            System.Console.WriteLine("Selecione uma opcao");
+            short opcao = short.Parse(Console.ReadLine());
+            System.Console.WriteLine();
+            switch (opcao)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Multiplicacao(); break;
+                case 4: Divisao(); break;
+                case 5: Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+
+
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor");
+            float v2 = float.Parse(Console.ReadLine());
+
+            System.Console.WriteLine();
+
+            float resultado = v1 + v2;
+            System.Console.WriteLine("O resultado da soma e: " + resultado);
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            System.Console.WriteLine("Primeiro Valor");
+            float v1 = float.Parse(Console.ReadLine());
+            System.Console.WriteLine("Segungo Valor");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 - v2;
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("O resultado e: " + resultado);
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            System.Console.WriteLine("Primeiro valor");
+            float v1 = float.Parse(Console.ReadLine());
+            System.Console.WriteLine("Segundo Valor");
+            float v2 = float.Parse(Console.ReadLine());
+            float resultado = v1 * v2;
+            System.Console.WriteLine();
+            System.Console.WriteLine("O resultado e: " + resultado);
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+            System.Console.WriteLine("Primeiro Valor");
+            float v1 = float.Parse(Console.ReadLine());
+            System.Console.WriteLine("Segundo Valor");
+            float v2 = float.Parse(Console.ReadLine());
+            float resultado = v1 / v2;
+            System.Console.WriteLine();
+            System.Console.WriteLine("O resultado e: " + resultado);
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
---
+++ Stopwatch/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            System.Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            System.Console.WriteLine("M = Minuto => 10m = 10 minutos");
+            System.Console.WriteLine("0 = Sair");
+            System.Console.WriteLine("Quanto tempo deseja contar?");
+
+            string opcao = Console.ReadLine().ToLower();
+            char type = char.Parse(opcao.Substring((opcao.Length - 1), 1));
+            int time = int.Parse(opcao.Substring(0, opcao.Length - 1));
+            int multiplier = 1;
+            if (type == 'm')
+                multiplier = 60;
+            if (time == 0)
+                Environment.Exit(time);
+
+            PreStart(time * multiplier);
+
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            System.Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            System.Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            Console.Clear();
+            int currentTime = 0;
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            System.Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
