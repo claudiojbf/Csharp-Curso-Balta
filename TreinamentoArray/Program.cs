76ed7663fe49a23e1368065cc02d20ef7a36ea37
Claudio Jose Bezerra Filho <65622389+claudiojbf@users.noreply.github.com>
2022-12-26 11:35:48 -0300

    Parte de Array Simples

---
+++ TreinamentoArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TreinamentoArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var meuArray = new int[5] { 10, 20, 30, 40, 50 };
+            // meuArray[0] = 12;
+            // System.Console.WriteLine(meuArray[0]);
+
+            for (int item = 0; item < meuArray.Length; item++)
+                System.Console.WriteLine(meuArray[item]);
+
+            foreach (var item in meuArray)
+                System.Console.WriteLine(item);
+
+
+        }
+    }
+}
