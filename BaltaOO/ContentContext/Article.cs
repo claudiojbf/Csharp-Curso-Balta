8488e495bbadaf6b11700af61805e83aa16f5c60
Claudio Jose Bezerra Filho <65622389+claudiojbf@users.noreply.github.com>
2022-12-27 11:37:29 -0300

    Começo da Orientação a Objeto Pratica

---
+++ BaltaOO/ContentContext/Article.cs
@@ -0,0 +1,7 @@
+namespace BaltaOO.ContentContext
+{
+    public class Article : Content
+    {
+
+    }
+}
---
+++ BaltaOO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaltaOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
