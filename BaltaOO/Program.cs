2bc9547968536d0173936468ac54476e370b3623
Claudio Jose Bezerra Filho <65622389+claudiojbf@users.noreply.github.com>
2022-12-27 14:05:43 -0300

    Interagindo com Lista

--- BaltaOO/Program.cs
+++ BaltaOO/Program.cs
@@ -18,6 +18,34 @@ namespace BaltaOO
                 System.Console.WriteLine(article.Title);
                 System.Console.WriteLine(article.Url);
             }
+            var Courses = new List<Course>();
+            var CourseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+            var CourseCsharp = new Course("Fundamentos c#", "fundamentos-c#");
+            var CourseDotnet = new Course("Fundamentos dotnet", "fundamentos-dotnet");
+
+            Courses.Add(CourseOOP);
+            Courses.Add(CourseCsharp);
+            Courses.Add(CourseDotnet);
+
+            var careers = new List<Career>();
+            var Career = new Career("Especialista Dotnet", "especialista-dotnet");
+            var careerItem2 = new CareerItem(2, "Aprenda .Net", "", null);
+            var careerItem = new CareerItem(1, "Comece por aqui", "", null);
+            var careerItem3 = new CareerItem(3, "OOP", "", null);
+
+            Career.Items.Add(careerItem2);
+            Career.Items.Add(careerItem);
+            Career.Items.Add(careerItem3);
+            careers.Add(Career);
+
+            foreach (var c in careers)
+            {
+                System.Console.WriteLine(c.Title);
+                foreach (var item in c.Items.OrderBy(x => x.Ordem))
+                {
+                    System.Console.WriteLine($"{item.Ordem} - {item.Title}");
+                }
+            }
         }
     }
 }
